108c8c0b35fece2337deb5227a8da5d6d7f31e6a
Ninan John <ninanjohn@IN-CHNI-Ninan-John.local>
2017-08-20 23:19:51 +0530

    #2 [Ninan] FIrst class, FIrst Adapter.

--- app/src/main/java/leo/orion/MainActivity.kt
+++ app/src/main/java/leo/orion/MainActivity.kt
@@ -2,13 +2,19 @@ package leo.orion
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import leo.orion.adapters.ForecastListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf("a","b","c")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hi"
+        val forecastList = findViewById(R.id.list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
