42bee9471ead8551774a07f11fb6fea36ee8adf7
Ninan John <ninanjohn@IN-CHNI-Ninan-John.local>
2017-08-25 20:48:32 +0530

    #3 [Ninan] Adding data classes and setting up JSON parsing(incomplete)

--- app/src/main/java/leo/orion/MainActivity.kt
+++ app/src/main/java/leo/orion/MainActivity.kt
@@ -5,16 +5,28 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import leo.orion.adapters.ForecastListAdapter
+import leo.orion.network.Request
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
 
 class MainActivity : AppCompatActivity() {
 
-    private val items = listOf("a","b","c")
+    private val items = listOf("a", "b", "c")
+
+    val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+            "APPID=2afad0c08fba13710457596c4f75921f&q=94043&mode=json&units=metric&cnt=7"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        doAsync(){
+            Request(url).run()
+            runOnUiThread { longToast("Request Performed") }
+        }
     }
 }
